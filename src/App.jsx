c72fb01ec96c254f577a5bf6ec32084d83e76a80
mehedi hasan khairul <mhkhairul78@gmail.com>
2023-11-03 04:06:06 +0600

    new version begun

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import GalleryNav from "./components/GalleryNav";
+
+function App() {
+  return (
+    <div>
+      <GalleryNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GalleryNav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GalleryNav = () => {
+  return <nav className="py-4 px-6"></nav>;
+};
+
+export default GalleryNav;
